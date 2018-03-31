aaaf794192ee51be1b2c916e5c71e159af3d92fc
Siltnamis <antilogic963@gmail.com>
2018-04-01 01:03:38 +0300

    add msvc build

---
+++ win64build.bat
@@ -0,0 +1,15 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+chdir /d %~dp0
+
+set include_dirs=/I"C:/dev/libs/" ^
+    /I"C:/dev/libs/sdl2/include/" ^
+    /I"C:/dev/libs/glad/include/"
+
+set link_path=/LIBPATH:"C:/dev/libs/sdl2/lib/x64" /LIBPATH:"C:/dev/libs/glad/static/"
+set link_libs=SDL2.lib SDL2main.lib glad.lib
+
+cl src/main.cpp /Fegame.exe %include_dirs% /DAV_GL_GLAD ^
+    /EHsc ^
+    /link %link_path%  %link_libs% /SUBSYSTEM:WINDOWS
+
+@rem /EHsc /wd4477 /wd4838 ^
