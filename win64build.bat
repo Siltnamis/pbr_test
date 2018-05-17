180baf024ad901c7b986449727a29bc2df49869e
Siltnamis <antilogic963@gmail.com>
2018-05-17 20:28:42 +0300

    add binary

--- win64build.bat
+++ win64build.bat
@@ -9,5 +9,5 @@ set link_path=/LIBPATH:"C:/dev/libs/sdl2/lib/x64" /LIBPATH:"C:/dev/libs/glad/sta
 set link_libs=SDL2.lib SDL2main.lib glad.lib
 
 cl src/main.cpp /Fegame.exe %include_dirs% /DAV_GL_GLAD ^
-    /EHsc /Zi ^
+    /EHsc /Zi /MT ^
     /link %link_path%  %link_libs% /SUBSYSTEM:WINDOWS
